317d976ff1c30e7bea9c5464131d526c8f4f861e
cwadven <cwadven@naver.com>
2025-06-07 20:47:53 +0900

    Feat: Delete redis key

--- apply-secrets.ps1
+++ apply-secrets.ps1
@@ -20,7 +20,4 @@ kubectl apply -f k8s/base/mysqld-exporter/secret.yaml -n $NAMESPACE
 # Elasticsearch Secret 적용
 kubectl apply -f k8s/base/elasticsearch/secret.yaml -n $NAMESPACE
 
-# Redis Secret 적용
-kubectl apply -f k8s/base/redis/secret.yaml -n $NAMESPACE
-
 Write-Host "Secrets applied successfully to namespace $NAMESPACE" 
